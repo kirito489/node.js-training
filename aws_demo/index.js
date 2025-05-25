bc9b8a9f94b418286996fc108374016e8486375c
Rean Tsai <aobc55625@gmail.com>
2025-05-26 02:27:33 +0800

    20250519 Experss 框架與實作

---
+++ aws_demo/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+require('dotenv').config()
+
+const {
+  S3Client,
+  PutObjectCommand,
+  CreateBucketCommand,
+  DeleteObjectCommand,
+  DeleteBucketCommand,
+  paginateListObjectsV2,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
+
+const app = express();
+
+const s3 = new S3Client({
+	region: process.env.AWS_REGION,
+	credentials: {
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+	}
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log(`server running on port ${PORT}`);
+});
